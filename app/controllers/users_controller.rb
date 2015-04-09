79a033d3d0fbe74634a7b339d4a8a27d1d0a3f7e
Ahmed Abdelaal <ahmed1500@gmail.com>
2015-04-09 10:10:25 -0700

    add users views and actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      redirect_to root_path, notice: "Congratulations you're now in the system!"
+    else
+      render 'new'
+    end
+  end
+
 end
