d7a124ec41fafc733cb83b2a4e67fc277f7bb20e
Ahmed Abdelaal <ahmed1500@gmail.com>
2015-04-09 11:03:39 -0700

    add log_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
-    
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Welcome back!'
+    else
+      render 'new', notice: 'Something went wrong. Please try again'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'See you later!'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users, only: [:index, :new, :create]
+  resources :users, only: [:index, :create]
+  get 'sign_up' => 'users#new'
+  get 'log_in' => 'sessions#new'
+  post 'log_in' => 'sessions#create'
+  delete 'log_out' => 'sessions#destroy'
 end
