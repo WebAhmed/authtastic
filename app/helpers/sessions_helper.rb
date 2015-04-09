af468773f54fc7a67cb53656d93b054f9807434f
Ahmed Abdelaal <ahmed1500@gmail.com>
2015-04-09 12:18:00 -0700

    remove log_in(user) method from sessions-herler

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+    # negating the negative equals true. Here it's sort of of like asking "is the current user not not valid?""
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :create]
   get 'sign_up' => 'users#new'
-  get 'log_in' => 'sessions#new'
-  post 'log_in' => 'sessions#create'
-  delete 'log_out' => 'sessions#destroy'
+  get 'sign_in' => 'sessions#new'
+  post 'sign_in' => 'sessions#create'
+  delete 'sign_out' => 'sessions#destroy'
 end
